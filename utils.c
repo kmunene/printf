6cb802c2893eea41315486074f985850b62203d7
ShimwatiMoody <119269634+ShimwatiMoody@users.noreply.github.com>
2023-07-25 23:12:40 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -2,8 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated.
- * Dispaly the char
+ * @c: Char to be evaluated
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -16,12 +15,11 @@ int is_printable(char c)
 }
 
 /**
- * append_hexa_code - Append ascci in hexadecimal to buffer
+ * append_hexa_code - Append ASCII in hexadecimal to buffer
  * @buffer: Array of characters
  * @i: Index at which to start appending
- * @ascii_code: ASSCI CODE.
+ * @ascii_code: ASCII code
  *
- * Display code
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
@@ -41,9 +39,8 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verify if char a digit
- * @c: Char to be evaluated.
- * Display the digit
+ * is_digit - Verify if a char is a digit
+ * @c: Char to be evaluated
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
@@ -56,11 +53,10 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts number to specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
- * Display the specified size..
- * 
+ * convert_size_number - Casts number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
+ *
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
@@ -74,10 +70,9 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Casts number to specified size
+ * convert_size_unsgnd - Casts number to the specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
- * Display the casted number
  *
  * Return: Casted value of num
  */
