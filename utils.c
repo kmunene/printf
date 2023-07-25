ae3bdd2714ef26a54bf1076fb69b05cf49d9494c
ShimwatiMoody <119269634+ShimwatiMoody@users.noreply.github.com>
2023-07-25 23:42:14 +0300

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * is_printable - Evaluates if a char is printable
- * @c: Char to be evaluated
+ * is_printable - This function evaluates if a char is printable
+ * @c: The character to be evaluated.
+ * Displaay the character
  *
  * Return: 1 if c is printable, 0 otherwise
  */
@@ -20,6 +21,7 @@ int is_printable(char c)
  * @i: Index at which to start appending
  * @ascii_code: ASCII code
  *
+ * Display code
  * Return: Always 3
  */
 int append_hexa_code(char ascii_code, char buffer[], int i)
@@ -39,8 +41,9 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verify if a char is a digit
- * @c: Char to be evaluated
+ * is_digit - Verify if char a digit
+ * @c: Char to be evaluated.
+ * Display the digit
  *
  * Return: 1 if c is a digit, 0 otherwise
  */
@@ -53,10 +56,11 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
- *
+ * convert_size_number - Casts number to specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
+ * Display the specified size..
+ * 
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
@@ -70,9 +74,10 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Casts number to the specified size
+ * convert_size_unsgnd - Casts number to specified size
  * @num: Number to be casted
  * @size: Number indicating the type to be casted
+ * Display the casted number
  *
  * Return: Casted value of num
  */
