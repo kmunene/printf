8589ffb7ad42ca577312eacfc9dd7320eeb291bd
ShimwatiMoody <119269634+ShimwatiMoody@users.noreply.github.com>
2023-07-25 21:46:06 +0300

    Update size.c

--- size.c
+++ size.c
@@ -1,50 +1,25 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
- * Return: Precision.
+ * get_size - Calculates the size to cast the argument.
+ * @format: Formatted string containing the format specifier.
+ * @i: Pointer to the current index in the format string.
+ * Return: Size specifier for casting the argument.
  */
 int get_size(const char *format, int *i)
 {
-	int curr_d = *i + 1;
-	int siz = 0;
+    int curr_i = *i + 1;
+    int size = 0;
 
-	if (format[curr_d] == 'l')
-		siz = S_LONG;
-	else if (format[curr_d] == 'h')
-		siz = S_SHORT;
+    if (format[curr_i] == 'l')
+        size = S_LONG;
+    else if (format[curr_i] == 'h')
+        size = S_SHORT;
 
-	if (siz == 0)
-		*i = curr_d - 1;
-	else
-		*i = curr_d;
+    if (size == 0)
+        *i = curr_i - 1;
+    else
+        *i = curr_i;
 
-	return (siz);
-=======
- * get_size - This function calculates the size to cast the argument
- * @format: Formatted strng containing the format specifier
- * @i: Pointer to the current index in the format string
- * Return: Size specifier for casting the argument
- */
-int get_size(const char *format, int *i)
-{
-	int curr_i = *i + 1;
-	int size = 0;
-
-	if (format[curr_i] == '1')
-		size = S_LONG;
-	else if (format[curr_i] == 'h')
-		size = S_SHORT;
-	if (size == 0)
-		*i = curr_i - 1;
-	else
-		*i = curr_i;
-
-	return (size);
-
->>>>>>> 9b606aab157324e1ebaf3519f0e875f6392257fd
+    return (size);
 }
