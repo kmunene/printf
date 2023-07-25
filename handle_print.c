d701f47275dfc68b9eb133303880dded79c4a76a
ShimwatiMoody <119269634+ShimwatiMoody@users.noreply.github.com>
2023-07-25 23:34:20 +0300

    Update handle_print.c

--- handle_print.c
+++ handle_print.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * handle_print - This function prints argument based on its type.
- * @fmt: Formatted string containing the format specifier.
+ * handle_print - This funct.prints argument based on its type
+* @fmt: Formatted string containing the format specifier.
  * @list: List of arguments to be printed.
  * @ind: Pointer to the current index in the format string.
  * @buffer: Buffer array to handle print.
@@ -10,15 +9,12 @@
  * @width: Width specification for formatting.
  * @precision: Precision specification for formatting.
  * @size: Size specifier for formatting.
- *
- * Return: Number of characters printed on success, -1 on failure.
+ * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknown_len = 0;
-	int printed_chars = -1;
-
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -26,22 +22,17 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-	{
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-	}
 
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-
-		unknown_len += write(1, "%%", 1);
-
+		unknow_len += write(1, "%%", 1);
 		if (fmt[*ind - 1] == ' ')
-			unknown_len += write(1, " ", 1);
+			unknow_len += write(1, " ", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -51,11 +42,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-
-		unknown_len += write(1, &fmt[*ind], 1);
-
-		return (unknown_len);
+		unknow_len += write(1, &fmt[*ind], 1);
+		return (unknow_len);
 	}
-
 	return (printed_chars);
 }
